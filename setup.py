f816f6de2dddea2cbd3acf6d54006e898a1b7f32
V2CK3 <3229523750@qq.com>
2026-07-31 14:13:08 +0800

    recovery from engineai to legged_gym

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-    name='engineai_training',
+    name='legged_gym',
     version='1.0.0',
-    author='engineai',
+    author='Nikita Rudin',
     license="BSD-3-Clause",
     packages=find_packages(),
-    author_email='info@engineai.com.cn',
+    author_email='rudinn@ethz.ch',
     description='Isaac Gym environments for Legged Robots',
     install_requires=['isaacgym',
                       'rsl-rl',
