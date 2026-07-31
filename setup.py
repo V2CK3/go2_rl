0bc55b058db2967af1dd1fb565f362dcabdc9933
V2CK3 <3229523750@qq.com>
2026-07-31 10:33:58 +0800

    Add engineai zqsa01 env on top of legged_gym baseline.
    Integrate engineai_legged_gym base/zqsa01 stack (env, utils, scripts),
    zq_humanoid assets, sim2real export, and matching rsl_rl runner/PPO tweaks.

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-    name='legged_gym',
+    name='engineai_training',
     version='1.0.0',
-    author='Nikita Rudin',
+    author='engineai',
     license="BSD-3-Clause",
     packages=find_packages(),
-    author_email='rudinn@ethz.ch',
+    author_email='info@engineai.com.cn',
     description='Isaac Gym environments for Legged Robots',
     install_requires=['isaacgym',
                       'rsl-rl',
-                      'matplotlib']
+                      'tensorboard',
+                      'setuptools==59.5.0',
+                      'matplotlib',
+                      'numpy==1.23',
+                      'opencv-python',
+                      'tqdm',
+                      'opencv-python',
+                      'pygame',
+                      'mujoco',
+                      'mujoco-python-viewer']
 )
